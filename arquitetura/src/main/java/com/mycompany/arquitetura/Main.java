ab14ce18cafda0f7cbb389dc07432c954780e0f1
xandea <xandeebr@hotmail.com>
2021-03-04 15:01:54 -0300

    Setado os valores ideias para textura de solo = 1

---
+++ arquitetura/src/main/java/com/mycompany/arquitetura/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.arquitetura;
+
+/**
+ *
+ * @author xande
+ */
+public class Main {
+    public static void main(String[] args){
+        Solo s = new Solo(); 
+        System.out.println("Valor antes"+s.getCalcio());
+        System.out.println("Set solo = 1");
+        s.Valor_Solo(1);
+        System.out.println("Valor depois"+s.getCalcio()+"\n");
+        
+        System.out.println("Set solo = 2");
+        s.Valor_Solo(2);
+        System.out.println("Valor depois"+s.getCalcio());
+        
+    }
+}
---
+++ arquitetura/src/main/java/com/mycompany/arquitetura/Solo.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.arquitetura;
+
+/**
+ *
+ * @author xande
+ */
+public class Solo {
+    
+    private double fosforo;
+    private double potassio;
+    private double calcio;
+    private double magnesio;
+    private double enxofre;
+    private double aluminio;
+    private double hal;
+  
+    public Solo Valor_Solo(int valor_textura){
+        Solo s = new Solo();
+        if(valor_textura==1){
+            setFosforo(9.0);
+            setPotassio(0.35);
+            setCalcio(6);
+            setMagnesio(1.5);
+            setEnxofre(9);
+            setAluminio(0);  
+        }
+        else{
+            System.out.println("ERRO: Tipo de textura de solo");   
+        }
+        return s;
+    }
+
+    public double getFosforo() {
+        return fosforo;
+    }
+
+    public void setFosforo(double fosforo) {
+        this.fosforo = fosforo;
+    }
+
+    public double getPotassio() {
+        return potassio;
+    }
+
+    public void setPotassio(double potassio) {
+        this.potassio = potassio;
+    }
+
+    public double getCalcio() {
+        return calcio;
+    }
+
+    public void setCalcio(double calcio) {
+        this.calcio = calcio;
+    }
+
+    public double getMagnesio() {
+        return magnesio;
+    }
+
+    public void setMagnesio(double magnesio) {
+        this.magnesio = magnesio;
+    }
+
+    public double getEnxofre() {
+        return enxofre;
+    }
+
+    public void setEnxofre(double enxofre) {
+        this.enxofre = enxofre;
+    }
+
+    public double getAluminio() {
+        return aluminio;
+    }
+
+    public void setAluminio(double aluminio) {
+        this.aluminio = aluminio;
+    }
+
+    public double getHal() {
+        return hal;
+    }
+
+    public void setHal(double hal) {
+        this.hal = hal;
+    }
+    
+   
+}
