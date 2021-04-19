6bdfc7744e09c73a297f2ac01f5d56a7b5f57793
xandea <xandeebr@hotmail.com>
2021-04-19 20:08:47 -0300

    Diminuição na estrutura do CalculoDeNutrientesAadicionar.java

--- arquitetura/src/main/java/com/mycompany/arquitetura/CalculoDeNutrientesAadicionar.java
+++ arquitetura/src/main/java/com/mycompany/arquitetura/CalculoDeNutrientesAadicionar.java
@@ -7,50 +7,22 @@ public class CalculoDeNutrientesAadicionar {
             double fosforo,//B11
             int fonteDeFostoroUtilizar,//D23
             double eficienciaDoFosforo){//D25
-        double teordeP2O5 = 0;
-        switch(fonteDeFostoroUtilizar){
-            case 1:
-                teordeP2O5=18.0;
-                break;
-            case 2:
-                teordeP2O5=41.0;
-                break;
-            case 3:
-                teordeP2O5=48.0;
-                break;
-            case 4:
-                teordeP2O5=45.0;
-                break;
-            case 5:
-                teordeP2O5=18.0;
-                break;
-            case 6:
-                teordeP2O5=33.0;
-                break;
-            case 7:
-                teordeP2O5=29.0;
-                break;
-            case 8:
-                teordeP2O5=32.0;
-                break;
-            case 9:
-                teordeP2O5=24.0;
-                break;
-            case 10:
-                teordeP2O5=18.5;
-                break;
-            case 11:
-                teordeP2O5=52.0;
-                break;
-            case 12:
-                teordeP2O5=18.0;
-                break;
-            default:   
-                System.out.println("ERRO: na inserção da Fonte de Fosforo a utilizar");
-                break;
-        }
-        double calculaQTDKgHectareDaFonteDeFosforo = 100/teordeP2O5*((teorFosforoAtingir-fosforo)*2*2.29*100/eficienciaDoFosforo/100); 
-        return calculaQTDKgHectareDaFonteDeFosforo;
+        double teordeP2O5 = switch(fonteDeFostoroUtilizar){
+            case 1-> 18.0;
+            case 2-> 41.0;
+            case 3-> 48.0;
+            case 4-> 45.0;
+            case 5-> 18.0;
+            case 6-> 33.0;
+            case 7-> 29.0;
+            case 8-> 32.0;
+            case 9-> 24.0;
+            case 10-> 18.5;
+            case 11-> 52.0;
+            case 12-> 18.0;
+            default-> 0.0;
+        };
+        return 100/teordeP2O5*((teorFosforoAtingir-fosforo)*4.58/eficienciaDoFosforo);
     }
     
     double calculaKgAlqueiroDaFonteDeFosforo(//B24
@@ -58,15 +30,11 @@ public class CalculoDeNutrientesAadicionar {
             double fosforo,//B11
             int fonteDeFostoroUtilizar,//D23
             double eficienciaDoFosforo){
-        double calculaKgAlqueiroDaFonteDeFosforo=0.0;
-        
-        calculaKgAlqueiroDaFonteDeFosforo = calculaQTDKgHectareDaFonteDeFosforo(
+        return calculaQTDKgHectareDaFonteDeFosforo(
             teorFosforoAtingir,//D22
             fosforo,//B11
             fonteDeFostoroUtilizar,//D23
             eficienciaDoFosforo)*2.42;
-        
-        return calculaKgAlqueiroDaFonteDeFosforo;
     }
     
     double necessidadeDePotassioAdicionar(//F51
